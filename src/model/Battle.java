6c46ce9746bd03dfd14ac5bbbc34a0fc8a32868b
Yash Yadav <yashcr7@email.arizona.edu>
2015-05-01 13:49:17 -0700

    Updated Battle Class: Created method that computes the result

--- src/model/Battle.java
+++ src/model/Battle.java
@@ -3,7 +3,7 @@ package model;
 public class Battle {
 	Pokemon pokemon = null;
 	Character character = null;
-	int[] result = new int[4]; //stores run rate, catch rate, turns and result
+	int[] result = new int[4];
 	/*
 	 * constructor
 	 */
@@ -13,7 +13,22 @@ public class Battle {
 		result[0] = pokemon.getRunChance();
 		result[1] = pokemon.getCatchRate();
 		result[2] = pokemon.getMaxTurns();
+		//display();
 	}
-
+	/*
+	 * returns the result of the fight
+	 */
+	public int result(int catchRate, int runChance){
+		if(runChance > 100 || result[2] == 0){
+			return -1;    //pokemon ran away
+		}
+		
+		else if(catchRate >= 90){
+			return 1;    //pokemon caught
+		}
+		else 
+		return 0;
+	}
+	
 	
 }
