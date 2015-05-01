b7379cec9c3c7e16b442351b4a72e01f49c9cea6
Yash Yadav <yashcr7@email.arizona.edu>
2015-05-01 13:47:09 -0700

    Created Battle class

--- src/model/Battle.java
+++ src/model/Battle.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Battle {
+	Pokemon pokemon = null;
+	Character character = null;
+	int[] result = new int[4]; //stores run rate, catch rate, turns and result
+	/*
+	 * constructor
+	 */
+	public Battle(Pokemon p, Character c){
+		pokemon = p;
+		character = c;
+		result[0] = pokemon.getRunChance();
+		result[1] = pokemon.getCatchRate();
+		result[2] = pokemon.getMaxTurns();
+	}
 
+	
 }
