dadb43bd5901a37beaa7759f1061d6cf389c4626
Yash Yadav <yashcr7@email.arizona.edu>
2015-05-03 17:17:03 -0700

    Character Class

---
+++ src/controller/Character.java
@@ -0,0 +1,47 @@
+package controller;
+
+import model.Inventory;
+
+public class Character {
+	String name;
+	Inventory inventory = null;
+	int steps;
+	//static Sprite sprite= null;
+	
+	/*
+	 * 
+	 */
+	public Character(String n, Inventory i, int s){
+		name = n;
+		inventory= i;
+		steps = s;
+		
+	}
+	public String getName(){
+		return name;
+	}
+	/*
+	 * 
+	 */
+	public Inventory getInventory(){
+		return inventory;
+	}
+	/*
+	 * 
+	 */
+	public int getSteps(){
+		return steps;
+	}
+	
+	/*
+	 * 
+	 */
+	public void updateSteps(int step){
+		steps+=step;
+	}
+
+	
+	
+	
+
+}
