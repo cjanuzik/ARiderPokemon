85a8bec678ce6105803e4bbfc48e24cb5dcf57e3
Yash Yadav <yashcr7@email.arizona.edu>
2015-05-03 17:57:29 -0700

    Updated character class

--- src/model/Character.java
+++ src/model/Character.java
@@ -4,11 +4,9 @@ package model;
 public class Character {
 	String name;
 	Inventory inventory = null;
-	int steps;
-	//static Sprite sprite= null;
-	
+	int steps;	
 	/*
-	 * 
+	 * Constructor to set values
 	 */
 	public Character(String n, Inventory i, int s){
 		name = n;
@@ -16,8 +14,30 @@ public class Character {
 		steps = s;
 		
 	}
+	/*
+	 * returns the name 
+	 */
 	public String getName(){
 		return name;
+	}
+	/*
+	 * adds pokemon to the inventory
+	 */
+	public void addPokemon(Pokemon p){
+		inventory.addPokemon(p);
+		
+	}
+	/*
+	 * adds surfing ability to the character
+	 */
+	public void addSurfAbility(){
+		inventory.setSurferUnlocked(true);
+	}
+	/*
+	 * adds rock destroyer ability to the character
+	 */
+	public void addRockDestroyer(){
+		inventory.setRockDestroyer(true);
 	}
 	/*
 	 * 
@@ -26,14 +46,14 @@ public class Character {
 		return inventory;
 	}
 	/*
-	 * 
+	 * returns the steps 
 	 */
 	public int getSteps(){
 		return steps;
 	}
 	
 	/*
-	 * 
+	 * updates the steps 
 	 */
 	public void updateSteps(int step){
 		steps+=step;
